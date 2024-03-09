509293d59fe97a6c3421760af200ac3d2fc1c63c
Ryan Kirkpatrick <ryan.kptk@outlook.com>
2024-03-09 10:31:45 +1000

    Changed port

--- App.js
+++ App.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.send('Successful response.');
 });
 
-app.listen(2211, () => {console.log("Running on localhost:2211.")});
+app.listen(80, () => {console.log("Running on localhost:80.")});
